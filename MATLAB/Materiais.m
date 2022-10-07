65d6777b13f3309bbc32f24a5151e20cdfc4e26e
Danilo Veras <dveras2310@gmail.com>
2022-10-07 01:18:31 -0300

    Incremento

--- MATLAB/Materiais.m
+++ MATLAB/Materiais.m
@@ -25,33 +25,33 @@ PorcFerrita2 = [0.022 0];
 TempFerrita2 = [727 912];
 
 PorcAustenita1 = [0:0.1:0.77];
-TempAustenita1 = [320*(PorcAustenita1-0.76)^2+727];
+TempAustenita1 = [320*(PorcAustenita1-0.77).^2+727];
 
 PorcAustenita2 = [0.77 2.14];
 TempAustenita2 = [727 1147];
 
 PorcAustenita3 = [2.14 0.16];
-TempAustenita3 = [1147 1493];
+TempAustenita3 = [1147 1515];
 
 PorcAustenita4 = [0.16 0];
-TempAustenita4 = [1493 1394];
+TempAustenita4 = [1515 1394];
 
-PorcPeripetica = [0.1 0.51];
-TempPeripetica = [1493 1493];
+PorcPeripetica = [0.1 0.494];
+TempPeripetica = [1515 1515];
 
 PorcFerritaDel1 = [0 0.1];
-TempFerritaDel1 = [1534 1496];
+TempFerritaDel1 = [1538 1515];
 
 PorcFerritaDel2 = [0.1 0];
-TempFerritaDel2 = [1493 1394];
+TempFerritaDel2 = [1515 1394];
 
 PorcFerritaDel3 = [0 0.16];
-TempFerritaDel3 = [1394 1493];
+TempFerritaDel3 = [1394 1515];
 
 PorcAusLiq = [0:0.1:4.30];
-TempAusLiq = [188.5*(-PorcAusLiq+4.3)^0.5+1147];
+TempAusLiq = [188.5*(-PorcAusLiq+4.3).^0.5+1147];
 
 PorcFe3CLiq = [4.3:0.1:6.7];
-TempFe3CLiq = [77*(PorcFe3CLiq-4.3)^0.5+1147];
+TempFe3CLiq = [77*(PorcFe3CLiq-4.3).^0.5+1147];
 
-plot(PorcEutetica, TempEutetica)
+plot(PorcEutetica, TempEutetica, PorcEutetoide, TempEutetoide, PorcFerrita1, TempFerrita1, PorcFerrita2, TempFerrita2, PorcAustenita1, TempAustenita1, PorcAustenita2, TempAustenita2, PorcAustenita3, TempAustenita3, PorcAustenita4, TempAustenita4, PorcPeripetica, TempPeripetica, PorcFerritaDel1, TempFerritaDel1, PorcFerritaDel2, TempFerritaDel2, PorcFerritaDel3, TempFerritaDel3, PorcAusLiq, TempAusLiq, PorcFe3CLiq, TempFe3CLiq)
