51b2f032944ac4683d8618bb9ea548ff0f53ab2a
Danilo Veras <dveras2310@gmail.com>
2022-10-06 22:40:11 -0300

    Versão 1.0

---
+++ MATLAB/Materiais.m
@@ -0,0 +1,13 @@
+disp('Programa de Categorização de Ligas em Detrimento da Porcentagem de Carbono')
+C = input('Qual a Porcentagem de Carbono da Liga? ')
+if C < 0.77 && C > 0.022
+    disp('Aço Hipoeutetoide')
+elseif C == 0.77
+    disp('Aço Eutetoide')
+elseif C <= 2.11 && C > 0.77
+    disp('Aço Hipereutetoide')
+elseif C <= 6.7 && C > 2.11
+    disp('Ferro Fundido')
+else
+    disp('Essa porcentagem não está dentro do intervalo 0,022 < %C < 6,7 ou não foi reconhecida.')
+end
