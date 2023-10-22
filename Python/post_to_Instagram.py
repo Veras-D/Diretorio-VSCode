9c65c0b0a43d7d90c8f47f172cb6164ccf684c38
Veras-D <dveras2310@gmail.com>
2023-10-22 00:03:54 -0300

    Posts to Instagram

---
+++ Python/post_to_Instagram.py
@@ -0,0 +1,61 @@
+# Post_001
+modulo_0 = 5 % 7
+print(f'O resultado é {modulo_0}.')
+
+### Post_002
+for i in range(5, 2, -1):
+    print(i)
+
+# Post_003
+x = 10
+y = x
+x = 5
+if x > y:
+    print('x')
+elif y > x:
+    print('y')
+    
+# Post_004
+def foo():
+    return "Olá, mundo!"
+
+print(foo())
+# a) foo()
+# b) "Olá, mundo!"
+# c) Olá, mundo!
+# d) Nada será impresso
+
+# Post_005
+palavra = "Python"
+print(palavra[1:4])
+
+# Post_006
+x = 10
+def funcao():
+    x = 5
+    return x
+print(x)
+print(funcao())
+
+# post_007
+def funcao(a=1, b=2):
+    return a + b
+
+resultado = funcao(b=3, a=4)
+print(resultado)
+
+# post_008
+
+# post_009
+
+# post_010
+
+# post_011
+
+# post_012
+
+# post_13
+
+#post_014
+
+# post_015
