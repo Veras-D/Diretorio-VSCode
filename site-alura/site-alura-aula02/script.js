84cb69c75b0955f687093f4e0eaa988769126f69
Veras-D <dveras2310@gmail.com>
2023-11-14 15:39:12 -0300

    Alura jogo JS update

--- site-alura/site-alura-aula02/script.js
+++ site-alura/site-alura-aula02/script.js
@@ -1,6 +1,6 @@
 var num = parseInt(Math.random() * 101);
 numPessoa = -1;
-
+cont = 1
 while (num != numPessoa) {
     var numPessoa = prompt('Digite um numero entre 1 e 100: ');
     if (num == numPessoa) {
@@ -9,10 +9,12 @@ while (num != numPessoa) {
     else {
         alert('Errou!')
         if (num > numPessoa) {
-            alert('O numero secreto é maior que seu chute');
+            alert('O numero secreto é maior que seu chute, '+numPessoa);
         }
         else {
-            alert('O numero secreto é menor que seu chute');
+            alert('O numero secreto é menor que seu chute, '+numPessoa);
         }
     }
+    alert('Seu numero de tentativas é: '+cont)
+    cont++
 }
