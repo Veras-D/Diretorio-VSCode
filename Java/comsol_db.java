965bd21798518de0ff23e3e45a8d744d0b4fe0f1
Veras-D <dveras2310@gmail.com>
2024-01-12 12:47:28 -0300

    COMSOL update

--- Java/comsol_db.java
+++ Java/comsol_db.java
@@ -43,7 +43,88 @@ for (int x1 = 30; x1 <= 150; x1 += 40) {
         // Fim Geometria
 
         // Computando
-        
+        model.sol("sol1").study("std1");
+        model.study("std1").feature("time").set("notlistsolnum", 1);
+        model.study("std1").feature("time").set("notsolnum", "auto");
+        model.study("std1").feature("time").set("listsolnum", 1);
+        model.study("std1").feature("time").set("solnum", "auto");
+        model.sol("sol1").feature().remove("t1");
+        model.sol("sol1").feature().remove("v1");
+        model.sol("sol1").feature().remove("st1");
+        model.sol("sol1").create("st1", "StudyStep");
+        model.sol("sol1").feature("st1").set("study", "std1");
+        model.sol("sol1").feature("st1").set("studystep", "time");
+        model.sol("sol1").create("v1", "Variables");
+        model.sol("sol1").feature("v1").set("control", "time");
+        model.sol("sol1").create("t1", "Time");
+        model.sol("sol1").feature("t1").set("tlist", "range(0,1,60)");
+        model.sol("sol1").feature("t1").set("plot", "off");
+        model.sol("sol1").feature("t1").set("plotgroup", "pg1");
+        model.sol("sol1").feature("t1").set("plotfreq", "tout");
+        model.sol("sol1").feature("t1").set("probesel", "all");
+        model.sol("sol1").feature("t1").set("probes", new String[]{});
+        model.sol("sol1").feature("t1").set("probefreq", "tsteps");
+        model.sol("sol1").feature("t1").set("atolglobalvaluemethod", "factor");
+        model.sol("sol1").feature("t1").set("atolmethod", new String[]{"comp1_T", "global"});
+        model.sol("sol1").feature("t1").set("atol", new String[]{"comp1_T", "1e-3"});
+        model.sol("sol1").feature("t1").set("atolvaluemethod", new String[]{"comp1_T", "factor"});
+        model.sol("sol1").feature("t1").set("atolvaluemethod", new String[]{"comp1_T", "factor"});
+        model.sol("sol1").feature("t1").set("reacf", true);
+        model.sol("sol1").feature("t1").set("storeudot", true);
+        model.sol("sol1").feature("t1").set("endtimeinterpolation", true);
+        model.sol("sol1").feature("t1").set("estrat", "exclude");
+        model.sol("sol1").feature("t1").set("maxorder", 2);
+        model.sol("sol1").feature("t1").set("control", "time");
+        model.sol("sol1").feature("t1").create("fc1", "FullyCoupled");
+        model.sol("sol1").feature("t1").feature("fc1").set("jtech", "once");
+        model.sol("sol1").feature("t1").feature("fc1").set("damp", 0.9);
+        model.sol("sol1").feature("t1").feature("fc1").set("stabacc", "aacc");
+        model.sol("sol1").feature("t1").feature("fc1").set("aaccdim", 5);
+        model.sol("sol1").feature("t1").feature("fc1").set("aaccmix", 0.9);
+        model.sol("sol1").feature("t1").feature("fc1").set("aaccdelay", 1);
+        model.sol("sol1").feature("t1").create("d1", "Direct");
+        model.sol("sol1").feature("t1").feature("d1").set("linsolver", "pardiso");
+        model.sol("sol1").feature("t1").feature("d1").set("pivotperturb", 1.0E-13);
+        model.sol("sol1").feature("t1").feature("d1").label("Direct, heat transfer variables (ht)");
+        model.sol("sol1").feature("t1").create("i1", "Iterative");
+        model.sol("sol1").feature("t1").feature("i1").set("linsolver", "gmres");
+        model.sol("sol1").feature("t1").feature("i1").set("prefuntype", "left");
+        model.sol("sol1").feature("t1").feature("i1").set("itrestart", 50);
+        model.sol("sol1").feature("t1").feature("i1").set("rhob", 20);
+        model.sol("sol1").feature("t1").feature("i1").set("maxlinit", 10000);
+        model.sol("sol1").feature("t1").feature("i1").set("nlinnormuse", "on");
+        model.sol("sol1").feature("t1").feature("i1").label("AMG, heat transfer variables (ht)");
+        model.sol("sol1").feature("t1").feature("i1").create("mg1", "Multigrid");
+        model.sol("sol1").feature("t1").feature("i1").feature("mg1").set("prefun", "saamg");
+        model.sol("sol1").feature("t1").feature("i1").feature("mg1").set("mgcycle", "v");
+        model.sol("sol1").feature("t1").feature("i1").feature("mg1").set("maxcoarsedof", 50000);
+        model.sol("sol1").feature("t1").feature("i1").feature("mg1").set("strconn", 0.01);
+        model.sol("sol1").feature("t1").feature("i1").feature("mg1").set("nullspace", "constant");
+        model.sol("sol1").feature("t1").feature("i1").feature("mg1").set("usesmooth", false);
+        model.sol("sol1").feature("t1").feature("i1").feature("mg1").set("saamgcompwise", true);
+        model.sol("sol1").feature("t1").feature("i1").feature("mg1").set("loweramg", true);
+        model.sol("sol1").feature("t1").feature("i1").feature("mg1").feature("pr").create("so1", "SOR");
+        model.sol("sol1").feature("t1").feature("i1").feature("mg1").feature("pr").feature("so1").set("iter", 2);
+        model.sol("sol1").feature("t1").feature("i1").feature("mg1").feature("pr").feature("so1").set("relax", 0.9);
+        model.sol("sol1").feature("t1").feature("i1").feature("mg1").feature("po").create("so1", "SOR");
+        model.sol("sol1").feature("t1").feature("i1").feature("mg1").feature("po").feature("so1").set("iter", 2);
+        model.sol("sol1").feature("t1").feature("i1").feature("mg1").feature("po").feature("so1").set("relax", 0.9);
+        model.sol("sol1").feature("t1").feature("i1").feature("mg1").feature("cs").create("d1", "Direct");
+        model.sol("sol1").feature("t1").feature("i1").feature("mg1").feature("cs").feature("d1")
+            .set("linsolver", "pardiso");
+        model.sol("sol1").feature("t1").feature("i1").feature("mg1").feature("cs").feature("d1")
+            .set("pivotperturb", 1.0E-13);
+        model.sol("sol1").feature("t1").feature("fc1").set("linsolver", "d1");
+        model.sol("sol1").feature("t1").feature("fc1").set("jtech", "once");
+        model.sol("sol1").feature("t1").feature("fc1").set("damp", 0.9);
+        model.sol("sol1").feature("t1").feature("fc1").set("stabacc", "aacc");
+        model.sol("sol1").feature("t1").feature("fc1").set("aaccdim", 5);
+        model.sol("sol1").feature("t1").feature("fc1").set("aaccmix", 0.9);
+        model.sol("sol1").feature("t1").feature("fc1").set("aaccdelay", 1);
+        model.sol("sol1").feature("t1").feature().remove("fcDef");
+        model.sol("sol1").attach("std1");
+        model.sol("sol1").runAll();
+        model.result("pg1").run();
         // Fim Computando 
         
         // Max value Temperatura
