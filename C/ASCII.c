5c8638084d699e40a2c5e6111eb9cc6a6a74aea9
Veras-D <dveras2310@gmail.com>
2024-02-14 14:17:16 -0300

    testing norminette 42

--- C/ASCII.c
+++ C/ASCII.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ASCII.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: veras <veras@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/14 14:07:34 by veras             #+#    #+#             */
+/*   Updated: 2024/02/14 14:17:07 by veras            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 
 int	main(void)
@@ -6,7 +18,6 @@ int	main(void)
 
 	character = 'a';
 	printf("O código ASCII do %c é: %d\n", character, character);
-
 	character = 'A';
 	printf("O código ASCII do %c é: %d\n", character, character);
 	return (0);
