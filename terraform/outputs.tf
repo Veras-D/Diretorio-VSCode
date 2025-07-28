a16a73f48ec682f32c501257938cd6f6a3cd1180
Veras-D <dveras2310@gmail.com>
2025-07-28 02:57:03 -0300

    add terraform locals studies

---
+++ terraform/outputs.tf
@@ -0,0 +1,15 @@
+output "bucket_name" {
+  value = aws_s3_bucket.this.bucket
+}
+
+output "bucket_arn" {
+  value = aws_s3_bucket.this.arn
+}
+
+output "bucket_domain_name" {
+  value = aws_s3_bucket.this.bucket_domain_name
+}
+
+output "ips_file_path" {
+  value = "${aws_s3_bucket.this.bucket}/${aws_s3_bucket.this.key}"
+}
