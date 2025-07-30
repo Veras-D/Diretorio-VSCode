c5bb1dd9604258cdbc7fd78b3ec34477321a1ea4
Veras-D <dveras2310@gmail.com>
2025-07-30 02:38:21 -0300

    add terraform data source studies

--- terraform/data.tf
+++ terraform/data.tf
@@ -3,3 +3,15 @@ data "aws_ami" "ubuntu" {
   most_recent = true
   name_regex = "ubuntu"
 }
+
+data "aws_caller_identity" "current" {}
+
+data "terraform_remote_state" "server" {
+  backend = "s3"
+
+  config = {
+    bucket = "tfstate-${data.aws_caller_identity.current.account_id}"
+    key    = "./terraform.tfstate"
+    region = var.aws_region
+  }
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -11,8 +11,6 @@ provider "aws" {
   profile = var.aws_profile
 }
 
-data "aws_caller_identity" "current" {}
-
 resource "aws_s3_bucket" "remote-state" {
   bucket = "tfstate-${data.aws_caller_identity.current.account_id}"
 }
